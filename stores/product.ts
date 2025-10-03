dd7c57099bf0117e27233bc2defa5058053a0934
poruethai <157833876+poruethai@users.noreply.github.com>
2025-10-04 05:23:48 +0700

    new

--- stores/product.ts
+++ stores/product.ts
@@ -14,12 +14,7 @@ export const useProductStore = defineStore('product', {
       const { data } = await axios.get('https://68d35a39214be68f8c6589da.mockapi.io/api/v1/store')
       this.products = data
     },
-    removeFromCart(id: string) {   
-      this.cart = this.cart.filter(c => c.id !== id)
-    },
-    clearCart() {   
-      this.cart = []
-    },
+
     addToCart(item:any) {
       this.cart.push(item)
     }
